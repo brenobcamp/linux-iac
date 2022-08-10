058a55e71c256cbf6cf7b17ccd38f757a4f75c5a
Breno Batista Campos <brenobatistacampos@gmail.com>
2022-08-10 14:16:36 -0300

    script

--- desafio-iac-linux.sh
+++ desafio-iac-linux.sh
@@ -3,6 +3,12 @@
 echo "Criando diretórios..."
 mkdir /publico /adm /ven /sec
 
+echo "Criando grupos..."
+
+groupadd GRP_ADM 
+groupadd GRP_VEN 
+groupadd GRP_SEC
+
 echo "Criando usuários..."
 useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd "123")
 passwd carlos -e 
@@ -30,3 +36,30 @@ passwd amanda -e
 
 useradd rogerio -c "Rogério" -s /bin/bash -m -p $(openssl passwd "123")
 passwd rogerio -e 
+
+echo "Atribuindo grupos aos usuários..."
+
+usermod -G GRP_ADM carlos
+usermod -G GRP_ADM maria
+usermod -G GRP_ADM joao
+
+usermod -G GRP_VEN debora
+usermod -G GRP_VEN sebastiana
+usermod -G GRP_VEN roberto
+
+usermod -G GRP_SEC josefina
+usermod -G GRP_SEC amanda
+usermod -G GRP_SEC rogerio
+
+
+echo "Definindo permissões..."
+
+chmod root:GRP_ADM /adm
+chmod root:GRP_VEN /ven
+chmod root:GRP_SEC /sec
+chmod root:root /publico
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
